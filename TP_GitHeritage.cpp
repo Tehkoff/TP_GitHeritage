883e2d1f215414ae976ee06cd2aeb27a406f1166
theo.mistral <mistheo73@gmail.com>
2022-10-06 17:21:41 +0200

    Ajout des bases des bases du cpp dans le fichier TP_GitHeritage.cpp

--- TP_GitHeritage.cpp
+++ TP_GitHeritage.cpp
@@ -1,9 +1,15 @@
+// By MISTRAL Théo 
+// Le 06-10-2022
 // TP_GitHeritage.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
+
+
+    return 0;
 }
